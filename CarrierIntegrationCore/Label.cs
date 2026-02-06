0018b789aedd105a95abc8d40aa91fc43e19818f
arnoldbono <roland.boon@live.nl>
2026-02-07 00:39:19 +0100

    Add domain model

---
+++ CarrierIntegrationCore/Label.cs
@@ -0,0 +1,9 @@
+namespace CarrierIntegrationCore;
+
+public class Label
+{
+    public string? Id { get; set; }
+    public required string ShipmentId { get; set; }
+    public required byte[] LabelData { get; set; } // PDF or base64 image data
+    public required string Format { get; set; } // e.g., "PDF", "PNG"
+}
---
+++ CarrierIntegrationCore/Shipment.cs
@@ -0,0 +1,10 @@
+﻿namespace CarrierIntegrationCore;
+
+public class Shipment
+{
+    public string? Id { get; set; }
+    public required string Carrier { get; set; } // e.g., "DHL", "FedEx", "UPS"
+    public required string TrackingNumber { get; set; }
+    public required double Amount { get; set; } // weight in kg
+    public required string Zone { get; set; } // e.g., "NL", "EU", "INT"
+}
